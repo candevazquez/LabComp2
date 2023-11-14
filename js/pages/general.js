f609e97bb3dfd9092b5d6545af099b6e8d81de5f
candevazquez <116578235+candevazquez@users.noreply.github.com>
2023-11-13 21:51:18 -0300

    subida

--- js/pages/general.js
+++ js/pages/general.js
@@ -424,22 +424,22 @@ async function filtrar() {
 
 
         // consulta api
-        const url = `https://elecciones-lc2.bruselario.com/api/resultados/getResultados?anioEleccion=${añoElegido}&tipoRecuento=${tipoRecuento}&tipoEleccion=${tipoEleccion}&categoriaId=${cargoElegido}&distritoId=${distritoElegido}&seccionProvincialId=${seccionProvincialId}&seccionId=${seccionElegida}&circuitoId=&mesaId=`;
+        const url = `https://resultados.mininterior.gob.ar/api/resultados/getResultados?anioEleccion=${añoElegido}&tipoRecuento=${tipoRecuento}&tipoEleccion=${tipoEleccion}&categoriaId=${cargoElegido}&distritoId=${distritoElegido}&seccionProvincialId=${seccionProvincialId}&seccionId=${seccionElegida}&circuitoId=&mesaId=`;
 
-        console.log('url= ', url)
+        console.log('url= ', url);
 
-        console.log(añoElegido)
-        console.log(tipoRecuento)
-        console.log(tipoEleccion)
-        console.log(cargoElegido)
-        console.log(distritoElegido)
-        console.log(seccionProvincialId)
-        console.log(seccionElegida)
+        console.log(añoElegido);
+        console.log(tipoRecuento);
+        console.log(tipoEleccion);
+        console.log(cargoElegido);
+        console.log(distritoElegido);
+        console.log(seccionProvincialId);
+        console.log(seccionElegida);
 
 
-//https://resultados.mininterior.gob.ar/api/resultados/getResultados?anioEleccion=2023&tipoRecuento=1&tipoEleccion=1&categoriaId=2&distritoId=5&seccionProvincialId=&seccionId=9&circuitoId=&mesaId=
-//https://elecciones-lc2.bruselario.com/
-//https://elecciones-lc2.bruselario.com/api/resultados/getResultados/?anioEleccion=2020&tipoRecuento=1&tipoEleccion=1&categoriaId=2&distritoId=1&seccionProvincialId=0&seccionId=3&circuitoId=000039&mesaId=1244
+        //https://resultados.mininterior.gob.ar/api/resultados/getResultados?anioEleccion=2023&tipoRecuento=1&tipoEleccion=1&categoriaId=2&distritoId=5&seccionProvincialId=&seccionId=9&circuitoId=&mesaId=
+        //https://elecciones-lc2.bruselario.com/
+        //https://elecciones-lc2.bruselario.com/api/resultados/getResultados/?anioEleccion=2020&tipoRecuento=1&tipoEleccion=1&categoriaId=2&distritoId=1&seccionProvincialId=0&seccionId=3&circuitoId=000039&mesaId=1244
 
         try {
             const respuesta = await fetch(url);
